e2d390665f0dbea51d9541e2d5dba62f2d81e1da
yyzx117 <97866407+yyzx117@users.noreply.github.com>
2023-07-17 14:34:17 +0000

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express')
 var app = express()
 app.disable("x-powered-by")
 app.get('/', function (req, res) {
-    res.send('Welcome to my webAPP!!')
+    res.send('Welcome to my webAPP!')
 })
 
 app.listen(3000, function() {
